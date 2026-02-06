42a44523abfc9cb41c51e14a35b6bdd6a92f6f20
Cesar Murillo <cesaryael.murillo@gmail.com>
2026-02-06 16:23:52 -0600

    Building Cart Store

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,24 +1,26 @@
-import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
+import toast from "react-hot-toast";
+import { useCartStore } from "../stores/useCartStore";
 import { useUserStore } from "../stores/useUserStore";
 
 const ProductCard = ({ product }) => {
 	const { user } = useUserStore();
+	const { addToCart } = useCartStore();
 	const handleAddToCart = () => {
 		if (!user) {
 			toast.error("Please login to add products to cart", { id: "login" });
 			return;
 		} else {
 			// add to cart
-			//addToCart(product);
+			addToCart(product);
 		}
 	};
 
 	return (
 		<div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
 			<div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
+				<div className="absolute inset-0 bg-black bg-opacity-20 opacity-0 group-hover:opacity-100 transition-opacity" />
 				<img className='object-cover w-full' src={product.image} alt='product image' />
-				<div className='absolute inset-0 bg-black bg-opacity-20' />
 			</div>
 
 			<div className='mt-4 px-5 pb-5'>
